ab9b66d55008bb0a25096145d8b5a0708eab7704
Aimery Javoy-Corbin <aimery.javoy@gmail.com>
2025-07-04 16:00:31 +0200

    fix : analytics

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -2,8 +2,8 @@ export const GA_TRACKING_ID = 'G-D3DD9KFEW3'
 
 // Suivi des pages
 export const pageview = (url: string) => {
-  if (typeof window !== 'undefined' && window.gtag) {
-    window.gtag('config', GA_TRACKING_ID, {
+  if (typeof window !== 'undefined' && 'gtag' in window) {
+    (window as any).gtag('config', GA_TRACKING_ID, {
       page_path: url,
     })
   }
@@ -21,8 +21,8 @@ export const event = ({
   label: string
   value: number
 }) => {
-  if (typeof window !== 'undefined' && window.gtag) {
-    window.gtag('event', action, {
+  if (typeof window !== 'undefined' && 'gtag' in window) {
+    (window as any).gtag('event', action, {
       event_category: category,
       event_label: label,
       value,
